8910883e746628d47ad8c805e5dc3bbdea4f8bc9
jrm <jeremyatn@gmail.com>
2023-05-05 18:51:06 +0200

    init + prisma setup with postgresql

---
+++ prisma/migrations/20230505163152_init/migration.sql
@@ -0,0 +1,43 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "image" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL,
+    "label" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "slug" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "authorId" TEXT NOT NULL,
+    "categoryId" TEXT NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_label_key" ON "Category"("label");
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ prisma/seed.ts
@@ -0,0 +1,85 @@
+import { faker } from "@faker-js/faker";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const main = async () => {
+  // 10 Users
+  const usersPromises = [];
+  for (let i = 0; i < 10; i++) {
+    const user = {
+      name: faker.name.fullName(),
+      email: faker.internet.email(),
+    };
+    usersPromises.push(
+      prisma.user.create({
+        data: user,
+      })
+    );
+  }
+
+  const users = await Promise.all(usersPromises);
+
+  // 5 Categories
+  const categoriesPromises = [];
+  for (let i = 0; i < 5; i++) {
+    const category = {
+      label: faker.word.adjective(),
+    };
+    categoriesPromises.push(
+      prisma.category.create({
+        data: category,
+      })
+    );
+  }
+
+  const categories = await Promise.all(categoriesPromises);
+
+  // 100 Posts
+  const postsPromises = [];
+  for (let i = 0; i < 100; i++) {
+    const randomUserIndex = faker.datatype.number({
+      min: 0,
+      max: users.length - 1,
+    });
+
+    const randomCategoryIndex = faker.datatype.number({
+      min: 0,
+      max: categories.length - 1,
+    });
+
+    const randomWorldCount = faker.datatype.number({
+      min: 25,
+      max: 50,
+    });
+
+    const post = {
+      title: faker.lorem.sentence(3),
+      content: faker.lorem.sentence(randomWorldCount),
+      slug: faker.lorem.slug(3),
+      authorId: users[randomUserIndex].id,
+      categoryId: categories[randomCategoryIndex].id,
+    };
+
+    postsPromises.push(
+      prisma.post.create({
+        data: post,
+      })
+    );
+  }
+
+  await Promise.all(postsPromises);
+};
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (error) => {
+    // eslint-disable-next-line no-console
+    console.error(error);
+
+    await prisma.$disconnect();
+
+    process.exit(1);
+  });
